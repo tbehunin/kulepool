7698a04a0123c26a6b7e971720f4f5994353ec49
Todd Behunin <tbehunin@gmail.com>
2016-06-22 16:16:37 -0700

    added a server component

---
+++ Server/Api/Controllers/PingController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    [RoutePrefix("api/ping")]
+    public class PingController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [Route("")]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [Route("")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [Route("")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
