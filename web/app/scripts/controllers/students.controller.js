6d3c957b239d641b0a08bed7317e616edea297c6
Todd Behunin <tbehunin@gmail.com>
2016-09-12 11:49:51 -0700

    stubbed out student stuff

--- web/app/scripts/controllers/students.controller.js
+++ web/app/scripts/controllers/students.controller.js
@@ -2,5 +2,27 @@
 
 module.exports = ['$scope', function ($scope) {
   var self = this;
-  self.hello = 'hello world!';
+  self.students = [
+    {
+      selected: false,
+      lastName: 'Behunin',
+      firstName: 'Ava',
+      grade: '7',
+      schoolName: 'Meadow Park Middle School'},
+    {
+      selected: false,
+      lastName: 'Behunin',
+      firstName: 'Eliza',
+      grade: '5',
+      schoolName: 'Meadow Park Middle School'},
+    {
+      selected: false,
+      lastName: 'Behunin',
+      firstName: 'Grant',
+      grade: '3',
+      schoolName: 'Meadow Park Middle School'}];
+  
+  self.select = function(student) {
+    student.selected = !student.selected;
+  };
 }];
