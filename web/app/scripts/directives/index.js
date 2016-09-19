f414ca71ab20771a84c6009379cab9b8545899c8
Todd Behunin <tbehunin@gmail.com>
2016-09-18 19:34:13 -0700

    Integrate multiselect

--- web/app/scripts/directives/index.js
+++ web/app/scripts/directives/index.js
@@ -2,3 +2,4 @@
 var app = require('angular').module('webApp');
 
 app.directive('blurOnClick', require('./blurOnClick.directive'));
+app.directive('multiselect', require('./multiselect.directive'));
