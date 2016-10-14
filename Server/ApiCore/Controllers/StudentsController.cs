7ac72f8115516d1f23ef398ded88bafa91007de9
Todd Behunin <tbehunin@gmail.com>
2016-10-14 15:41:16 -0700

    rename stuff

--- Server/ApiCore/Controllers/StudentsController.cs
+++ Server/ApiCore/Controllers/StudentsController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebCore.Controllers
+namespace ApiCore.Controllers
 {
     [Route("api/[controller]")]
     public class StudentsController : Controller
