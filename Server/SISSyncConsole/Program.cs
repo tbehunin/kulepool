4651c5ef1e8161c6962ee5e96671a5b2ef1da6c6
Todd Behunin <tbehunin@gmail.com>
2016-10-16 18:34:33 -0700

    new sync projects on .net core

---
+++ Server/SISSyncConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SISSyncConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+            Console.WriteLine("Finished. Press <enter> to exit.");
+            Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            //
+        }
+    }
+}
