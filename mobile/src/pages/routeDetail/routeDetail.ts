d42bca346a381f74acb317a758425b65b74f31b7
Todd Behunin <tbehunin@gmail.com>
2017-02-03 10:58:15 -0800

    stubbed out a native map!

--- mobile/src/pages/routeDetail/routeDetail.ts
+++ mobile/src/pages/routeDetail/routeDetail.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { Route } from './route'
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
 
 @Component({
+  selector: 'route-detail',
   templateUrl: 'routeDetail.html'
 })
 export class RouteDetail {
-
-    constructor(public navCtrl: NavController) {
+  map: GoogleMap;
+ 
+    constructor(public navCtrl: NavController, public platform: Platform) {
+        platform.ready().then(() => {
+            this.loadMap();
+        });
+    }
+ 
+    loadMap(){
+ 
+        let location = new GoogleMapsLatLng(-34.9290,138.6010);
+ 
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': true
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        });
+ 
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!');
+        });
+ 
     }
 }
